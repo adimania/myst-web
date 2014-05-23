d5a53d93e3ded714cdc822e69f49aba4abff2ddf
Aditya <aditya@adityapatawari.com>
2014-05-23 22:15:52 +0530

    first run for myst-web, a web based twitter client

--- README.md
+++ README.md
@@ -2,3 +2,21 @@ myst-web
 ========
 
 A slightly modified version of MYST project to tweet over web when twitter is not accessible 
+
+* Twitter is not available where I am right now, so I have created this small script that can be hosted on any server from where twitter can be accessed.
+* A POST request to /tweet will tweet.
+* A GET request to /timeline will fetch your timeline
+* A GET request to /search with query supplied in variable "q" will get search results. Example: /search?q=fudcon
+
+Requirements
+------------
+
+* pip install python-twitter
+* Your twitter creds in the following format
+```
+[MYST]
+consumer_key: <consumer_key>
+consumer_secret: <consumer_secret>
+access_token: <access_token>
+access_token_secret: <access_token_secret>
+```
---
+++ myst.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+# author: Aditya Patawari <aditya@adityapatawari.com>
+# A python-twitter based script
+
+import twitter
+import os.path
+import ConfigParser
+import sys
+import os
+import web
+
+__author__ = "Aditya Patawari <aditya@adityapatawari.com>"
+version = 0.25
+
+Description=''' MYST, short for My Status, is an ideal tool for status updates, DMs, searching etc.
+It does everything a normal user can expect.
+The Consumer Key and Secret Pair and Access Token Key and secret pair are stored in ~/.myst.conf
+The format for the file is : 
+    [MYST]
+    consumer_key: <Consumer Key>
+    consumer_secret: <Consumer Secret>
+    access_token: <Access Token Key>
+    access_token_secret: <Access Token Secret>
+'''
+
+urls = (
+  '/tweet','Tweet',
+  '/timeline','Timeline',
+  '/search','Search'
+)
+     
+check = os.path.isfile(os.path.expanduser('~/.myst.conf'))
+if cmp(check,False) == 0:
+    print Description
+    sys.exit(2)
+
+config = ConfigParser.ConfigParser()
+config.read(os.path.expanduser('~/.myst.conf'))
+conskey = config.get("MYST", "consumer_key", raw=True)
+conssec = config.get("MYST", "consumer_secret", raw=True)
+accstkn = config.get("MYST", "access_token", raw=True)
+accssec = config.get("MYST", "access_token_secret", raw=True)
+
+user=twitter.Api(consumer_key=conskey, consumer_secret=conssec, access_token_key=accstkn, access_token_secret=accssec)
+
+class Tweet:
+  def POST(self):
+    data=web.input()
+    user.PostUpdates(data['tweet'])
+
+class Timeline:
+  def GET(self):
+    data=web.input()
+    if 'user' in data.keys():
+      feeds = user.GetUserTimeline(data['user'])
+    else:
+      feeds = user.GetUserTimeline()
+    feed_str = "<p>"
+    for feed in feeds:
+       feed_str = feed_str + feed.text + "<br />"
+    return feed_str + "</p>"  
+
+class Search:
+  def GET(self):
+    data = web.input()
+    search=user.GetSearch(data['q'])
+    feed_str = "<p>"
+    for items in search:
+      feed_str = feed_str + getattr(s.user,"screen_name") + ": " + getattr(s,"text") + "<br />"
+    return feed_str + "</p>"
+
+app = web.application(urls, globals())
+
+if __name__ == '__main__':
+    app.run()
